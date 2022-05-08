12a85b4e2a67a321907e03b128b89b0f69e74240
aya1996 <aya.mohamed666fciscu@gmail.com>
2022-05-08 16:30:49 +0200

    first commit

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+
+    use RefreshDatabase;
+    public function test_login_redirect_to_home_successfully()
+    {
+        User::factory()->create([
+            'email' => 'test@test.com',
+            'password' => bcrypt('password')
+        ]);
+
+        $response = $this->post('/login', [
+            'email' => 'test@test.com',
+            'password' => 'password'
+        ]);
+        $response->assertStatus(302);
+        $response->assertRedirect('/home');
+    }
+
+    public function test_auth_user_can_access_home()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->get('/home');
+        $response->assertStatus(200);
+    }
+
+    public function test_unath_user_cannot_access_dasboard()
+    {
+        $response = $this->get('/home');
+        $response->assertStatus(302);
+        $response->assertRedirect('/login');
+    }
+
+    public function test_user_has_name_attribute()
+    {
+        $user = User::factory()->create([
+            'name' => 'John',
+            'email' => 'test@test.com',
+            'password' => bcrypt('password')
+        ]);
+
+        $this->assertEquals('John', $user->name);
+    }
+}
